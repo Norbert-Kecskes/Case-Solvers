57d84975fbfe1d8fb3586570b0acacb1270e5222
Norberto-DiCaprio <webfejlesztes.kn@gmail.com>
2018-12-01 09:27:24 +0100

    Email JavaScripttel.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-﻿
+﻿$(document).ready(function() {
+    $("#emailCheck").submit(function(ev) {
+      ev.preventDefault();
+
+      var emailCheckElem = $(this);
+      var emailCheckNativeElem = emailCheckElem[0];
+
+      function emailekHide() {
+          var emailek = $("#valosEmail, #valotlanEmail")
+          emailek.hide();
+      };
+
+      if (emailCheckNativeElem.checkValidity() == true) {
+            emailekHide();
+            $("#valosEmail").fadeIn();
+        } else {
+            emailekHide();
+            $("#valotlanEmail").fadeIn();
+        };
+
+      if (emailCheckElem.hasClass("was-validated") == false) {
+        emailCheckElem.addClass("was-validated");
+      }
+    });
+
+  });
